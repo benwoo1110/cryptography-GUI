f1cbe6bcd24dfd181de20f9d1e6308f4a5d6ba45
benwoo1110 <benedict_woo_jun_kai@s2016.sst.edu.sg>
2020-06-09 14:00:38 +0800

    Put .py files into code folder.

---
+++ code/polygram_subsitution_cipher.py
@@ -0,0 +1,18 @@
+##
+## Import and initialize the librarys
+import pygame
+from item_storage import *
+from pygame_ess import pygame_ess
+
+
+##
+## Initialization
+pygame.init()
+
+
+##
+## 
+class polygram_subsitution_cipher:
+    def run():        
+        while True:
+            if pygame_ess.buffer(): break
